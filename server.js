fc183f91cfd02d1eea33c9d004cbefc518c11701
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-06-19 21:17:33 -0600

    cambios

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes/routes');
+
+const app = express();
+const port = 8080;
+app.use(cors());
+// Conexión a la base de datos MongoDB Atlas
+mongoose.connect('mongodb+srv://alexandrubites:IUMD6wqtYGbVh0yw@cluster0.z4xurdo.mongodb.net',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: 'bdnosql' // Reemplaza con el nombre real de tu base de datos
+});
+
+// Middleware para analizar datos JSON en las solicitudes
+app.use(express.json());
+app.use('/api', routes);
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
